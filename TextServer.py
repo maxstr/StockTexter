fb5b3ba10ee532514c75a49ceae8791a7f9b526a
Max <max@lambda.mu>
2016-05-22 17:20:46 -0400

    More textserver updates

--- TextServer.py
+++ TextServer.py
@@ -3,22 +3,23 @@ import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
+@app.route("/", methods=['POST'])
+def stockResponse():
     """Respond to incoming calls with a simple text message."""
 
+    # Handle and parse incoming data
+    incomingText = request.form['Body']
+    stockList = incomingText.split()
+
+
+    #Return a response
+    returnLines = "\n"
+    for stock in stockList:
+        returnLines += "%s : %.2f\n" % (stock, 100.20)
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    print request
+    resp.message(returnLines)
+    
     return str(resp)
-
-@app.route("/stocks", methods=['POST'])
-def stockResponse():
-    """Returns a stock quote given a stock"""
-    pass
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
