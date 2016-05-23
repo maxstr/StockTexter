d6d884b5bfa8a468acd0719f55253601bbf53560
Max Stritzinger <mstritzinger@live.com>
2016-05-22 17:59:16 -0700

    object isn't going to work

--- TextServer.py
+++ TextServer.py
@@ -2,38 +2,34 @@ from flask import Flask, request, redirect
 import StockGrabber as sg
 import twilio.twiml
 
-HTTPServer = Flask(__name__)
+TextServer = Flask(__name__)
 
-class TextServer:
-    def __init__(self):
-        self.HTTPServer = HTTPServer
+@TextServer.route("/", methods=['POST'])
+def stockResponse():
+    """Respond to incoming calls with a simple text message."""
 
-    @HTTPServer.route("/", methods=['POST'])
-    def stockResponse(self):
-        """Returns a response to a text message"""
+    # Handle and parse incoming data
+    incomingText = request.form['Body']
+    stockList = incomingText.split()
 
-        # Handle and parse incoming data
-        incomingText = request.form['Body']
-        stockList = incomingText.split()
-
-        # Get the stock info we want.
-        stockInfo = stockInfoFromTickers(stockList, sg.FINANCE_PARAMS_BASIC)
+    # Get the stock info we want.
+    stockInfo = stockInfoFromTickers(stockList, sg.FINANCE_PARAMS_BASIC)
 
 
-        #Return a response
-        returnLines = ""
-        for stock in stockList:
-            returnLines += "%s \n" % stockInfo[stock]['Name']
-            for param in filter(lambda key: key != 'Name', sg.FINANCE_PARAMS_BASIC.values()):
-                returnLines += "%s : %s" % (param, stockInfo[stock][param])
-            returnLines += "\n"
+    #Return a response
+    returnLines = ""
+    for stock in stockList:
+        returnLines += "%s \n" % stockInfo[stock]['Name']
+        for param in filter(lambda key: key != 'Name', sg.FINANCE_PARAMS_BASIC.values())
+            returnLines += "%s : %s" % (param, stockInfo[stock][param])
+        returnLines += "\n"
 
 
 
-        resp = twilio.twiml.Response()
-        resp.message(returnLines)
+    resp = twilio.twiml.Response()
+    resp.message(returnLines)
 
-        return str(resp)
+    return str(resp)
 
 if __name__ == "__main__":
-    HTTPServer.run(debug=True, host='0.0.0.0')
+    TextServer.run(debug=True, host='0.0.0.0')
