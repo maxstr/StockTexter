aff87f2479882348b111c1a712a361514ea9043b
Max Stritzinger <mstritzinger@live.com>
2016-05-22 19:42:33 -0700

    how we want it

---
+++ StockTexter.py
@@ -0,0 +1,15 @@
+import TextServer as ts
+import ScheduleServer as ss
+import multiprocessing as mp
+
+if __name__ == '__main__':
+
+    TextServer = ts.TextServer
+    TServerP = mp.Process(target = TextServer.run, kwargs = \
+                { 'debug' : True, 'host': '0.0.0.0' })
+
+    ScheduleServer = ss.ScheduleServer()
+    SServerP = mp.Process(target = ScheduleServer.run)
+
+
+
