725b6ba4dd4ba8fa9ee4d4199ee6171a35fa58a1
Max Stritzinger <mstritzinger@live.com>
2016-05-22 17:53:55 -0700

    Turn TextServer into object

--- TextServer.py
+++ TextServer.py
@@ -2,38 +2,38 @@ from flask import Flask, request, redirect
 import StockGrabber as sg
 import twilio.twiml
 
-app = Flask(__name__)
+HTTPServer = Flask(__name__)
 
-@app.route("/", methods=['POST'])
-def stockResponse():
-    """Respond to incoming calls with a simple text message."""
+class TextServer:
+    def __init__(self):
+        self.HTTPServer = HTTPServer
 
-    # Handle and parse incoming data
-    incomingText = request.form['Body']
-    stockList = map(lambda stock: stock.upper(), incomingText.split())
+    @HTTPServer.route("/", methods=['POST'])
+    def stockResponse(self):
+        """Returns a response to a text message"""
 
-    # Get the stock info we want.
-    stockInfo = sg.stockInfoFromTickers(stockList, sg.FINANCE_PARAMS_BASIC)
+        # Handle and parse incoming data
+        incomingText = request.form['Body']
+        stockList = incomingText.split()
 
-
-    #Return a response
-    returnLines = ""
-    for stock in stockList:
-	if stockInfo[stock]['Name'] != 'N/A':
-		returnLines += "%s - %s \n" % (stockInfo[stock]['Name'], stock)
-		paramsNoName = filter(lambda key: key != 'Name', sg.FINANCE_PARAMS_BASIC.values())
-		for param in paramsNoName:
-		    returnLines += "%s : %s\n" % (param, stockInfo[stock][param])
-	else:
-		returnLines += "%s - Ticker could not be found" % stock
-        returnLines += "\n"
+        # Get the stock info we want.
+        stockInfo = stockInfoFromTickers(stockList, sg.FINANCE_PARAMS_BASIC)
 
 
+        #Return a response
+        returnLines = ""
+        for stock in stockList:
+            returnLines += "%s \n" % stockInfo[stock]['Name']
+            for param in filter(lambda key: key != 'Name', sg.FINANCE_PARAMS_BASIC.values()):
+                returnLines += "%s : %s" % (param, stockInfo[stock][param])
+            returnLines += "\n"
 
-    resp = twilio.twiml.Response()
-    resp.message(returnLines)
 
-    return str(resp)
+
+        resp = twilio.twiml.Response()
+        resp.message(returnLines)
+
+        return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    HTTPServer.run(debug=True, host='0.0.0.0')
